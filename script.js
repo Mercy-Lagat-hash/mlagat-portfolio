5302aba2bbd14f14691386c2a82903fcf146bb45
Mercy Lagat <mjl4@williams.edu>
2025-09-02 09:15:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let rotateFactor = 0;
-const modalImage = document.getElementById("modal-image");
- const animateImage = () => {
-  if (rotateFactor === 0) {
-    rotateFactor = -10;
-  } else {
-    rotateFactor = 0;
-  }
+const modalImage = document.getElementById("dancingImage");
+
+const animateImage = () => {
+  rotateFactor = rotateFactor === 0 ? -10 : 10; // swing left & right
   modalImage.style.transform = `rotate(${rotateFactor}deg)`;
- };
+};
+
+// run every 400ms
+setInterval(animateImage, 400);
+
